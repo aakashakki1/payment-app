b9e270f8ad4736e8ca8ee907a751f068bddfe2e6
Aakash <ak.1998.iert@gmail.com>
2020-10-30 15:59:42 +0530

    final localHost

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
@@ -8,15 +8,14 @@ const routes = require("./routes/initiatePayment");
 const pay = require("./services/pay");
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-let port = process.env.port;
-if (!port) {
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
+let host = "0.0.0.0";
+
 // connecting with database
 
 mongoose
@@ -33,11 +32,11 @@ mongoose
 
 app.set("views", path.join(__dirname, "./views")).set("view engine", "ejs");
 
-app.get("/index", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 // initiating payment
-app.use("/initiatePayment", routes());
+app.use("/initiatePayment", routes);
 // finsihing payment
 app.get("/client", (req, res) => {
   res.render("client.html", path.join(__dirname, "client"));
@@ -46,6 +45,6 @@ app.post("/", async (req, response) => {
   let status = await pay(req.body);
   return response.send(status);
 });
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Express server listening on port ${port}!`);
 });
