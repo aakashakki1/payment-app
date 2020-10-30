343993658123be14083704a8008064cf6ea62b15
Aakash <ak.1998.iert@gmail.com>
2020-10-30 10:14:28 +0530

    almost final

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const routes = require("./routes/initiatePayment");
 const pay = require("./services/pay");
@@ -17,6 +18,7 @@ if (!port) {
   port = 3000;
 }
 // connecting with database
+
 mongoose
   .connect(
     "mongodb+srv://ak:ROIIM123@cluster0.tht0j.mongodb.net/ROIIM?retryWrites=true&w=majority",
@@ -28,17 +30,22 @@ mongoose
   )
   .then(() => console.log("DATABASE CONNECTION SUCCESFULLY ESTABLISHED"))
   .catch((err) => console.log(err));
+
+app.set("views", path.join(__dirname, "./views")).set("view engine", "ejs");
+
+app.get("/index", (req, res) => {
+  res.render("index");
+});
 // initiating payment
 app.use("/initiatePayment", routes());
 // finsihing payment
+app.get("/client", (req, res) => {
+  res.render("client.html", path.join(__dirname, "client"));
+});
 app.post("/", async (req, response) => {
   let status = await pay(req.body);
   return response.send(status);
 });
-let port = process.env.port;
-if (port === null) {
-  port = 3000;
-}
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}!`);
 });
